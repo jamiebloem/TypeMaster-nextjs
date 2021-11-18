6bfe6872a950f93b2c94a175189850b3b514d44a
Jamie Bloem <jamiebloem@hotmail.com>
2021-11-18 16:37:32 +0100

    Remove build folder from git

---
+++ pages/TypingGame.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react";
+
+function TypingGame(){
+
+    const [post, setPost] = useState([]);
+
+    // useEffect(() =>
+
+
+
+    return(
+'Hello world'
+    )
+}
+
+export default TypingGame;
