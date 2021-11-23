55e0e92aead03877b497a971b32e68ca187f3890
Jamie Bloem <jamiebloem@hotmail.com>
2021-11-23 11:57:11 +0100

    Listen for events, store UserInput, give feedback

--- pages/TypingGame.js
+++ pages/TypingGame.js
@@ -2,7 +2,8 @@ import {useEffect, useState} from 'react';
 
 function TypingGame() {
     const [words, setWords] = useState([]);
-
+    const [currentWordIndex, setCurrentWordIndex] = useState(0);
+    const [userInput, setUserInput] = useState('');
     useEffect(() => {
 
         const getWords = async () => {
@@ -10,18 +11,23 @@ function TypingGame() {
             const res = await fetch('https://random-word-api.herokuapp.com/word?number=10');
             const data = await res.json();
             setWords(data)
-
         }
         getWords()
     }, [])
 
+    useEffect(() => {
+        window.addEventListener('keydown', (event) => {
+                setUserInput((previousUserInput) => previousUserInput + event.key
+                )
+            }
+        )
+    }, [])
+    console.log('input', userInput)
+    console.log('words', words)
     return <div>
-        <h1>Hi</h1>
-        {words.map(word => (
-            <div key={word}>
-                <h1> {word}</h1>
-            </div>
-        ))}
+        <p>{words[currentWordIndex]}</p>
+        <p> {userInput} </p>
+        <p>{words[currentWordIndex] === userInput && 'Good job!'}</p>
     </div>
 }
 
